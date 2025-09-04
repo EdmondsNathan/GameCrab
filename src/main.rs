126c93191c3a8db57acb7bdb19d420d859d29a13
Nathan <edmondsnathan1998@gmail.com>
2025-09-04 10:34:10 -0500

    Implement basic screen renderer
    For now this simply renders a gradient, but will pull data from the emulator once that is implemented

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,41 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+    let mut bytes: Box<[u8]> = Box::new([128u8; RESOLUTION.x as usize * RESOLUTION.y as usize * 4]);
+
+    let texture: Texture2D = Texture2D::from_rgba8(160, 144, &bytes);
+
     loop {
         clear_background(BLACK);
+
+        update_texture(&texture, &mut bytes);
+
+        render_texture(&texture);
 
         draw_fps();
 
         next_frame().await
     }
+}
+
+fn update_texture(texture: &Texture2D, bytes: &mut Box<[u8]>) {
+    for y in 0..RESOLUTION.y as usize {
+        for x in 0..RESOLUTION.x as usize {
+            let start = (y * 160 * 4) + (x * 4);
+            bytes[start..start + 4].copy_from_slice(&[x as u8, 0, y as u8, 255,]);
+        }
+    }
+
+    texture.update_from_bytes(160, 144, bytes);
+}
+
+fn render_texture(texture: &Texture2D) {
+    draw_texture_ex(texture, 0f32, 0f32, WHITE, DrawTextureParams {
+        dest_size: Some(SCALED_RESOLUTION),
+        rotation: 0f32,
+        flip_x: false,
+        flip_y: false,
+        pivot: None,
+        source: None
+    });
 }
