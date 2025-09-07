a3316bc69707f28cce5d96547b03c8328b7cc15e
Nathan <edmondsnathan1998@gmail.com>
2025-09-06 21:25:06 -0500

    Create cpu.rs

---
+++ src/emulator/cpu.rs
@@ -0,0 +1,24 @@
+use crate::emulator::registers::Registers;
+use crate::emulator::ram::RAM;
+
+pub struct CPU {
+    registers: Registers,
+    stack_pointer: u16,
+    program_counter: u16,
+    ram: RAM,
+}
+
+impl CPU {
+    pub fn new() -> CPU {
+        CPU {
+            registers: Registers::new(),
+            stack_pointer: 0,
+            program_counter: 0,
+            ram: RAM::new(),
+        }
+    }
+
+    pub fn test_overflow(&self) {
+        // self.ram.fetch_16(65535);
+    }
+}
