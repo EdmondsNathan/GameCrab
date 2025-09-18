89521c0abaebfc4c191d6f978a73358386941389
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-09-18 12:32:34 -0500

    remove dead code

--- src/emulator/console.rs
+++ src/emulator/console.rs
@@ -3,7 +3,6 @@ use crate::emulator::cpu::CPU;
 pub struct Console {
     cpu: CPU,
     tick_counter: u64,
-    // cpu_execution_queue: VecDeque<ExecutionRequest>,
 }
 
 impl Console {
@@ -11,31 +10,10 @@ impl Console {
         Console {
             cpu,
             tick_counter: 0,
-            // cpu_execution_queue: VecDeque::new(),
         }
     }
 
     pub fn tick(&mut self) {
         self.tick_counter += 1;
-
-        /*match self.cpu_execution_queue.pop_front() {
-            None => {
-                let byte = self.cpu.ram.fetch(self.cpu.program_counter);
-                let instruction = decode(byte);
-                match instruction {
-                    Ok(instruction) => {
-                        self.cpu.execute(instruction);
-                    }
-                    Err(error) => {
-                        panic!("{error}");
-                    }
-                }
-            }
-            Some(func) => match func {
-                ExecutionRequest::CPUTick(func) => {
-                    func(&mut self.cpu);
-                }
-            },
-        }*/
     }
 }
