10869d6cedf6818db67b9ea70d1d28c2bb4cc927
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-09-21 16:53:17 -0500

    Implement TickMap struct

--- src/emulator/tick_map.rs
+++ src/emulator/tick_map.rs
@@ -4,3 +4,26 @@ use std::collections::{HashMap, VecDeque};
 pub(crate) struct TickMap {
     map: HashMap<u64, VecDeque<fn(&mut Console)>>,
 }
+
+impl TickMap {
+    pub(crate) fn new() -> TickMap {
+        TickMap {
+            map: HashMap::new(),
+        }
+    }
+
+    pub(crate) fn push_command(&mut self, tick: u64, command: fn(&mut Console)) {
+        match self.map.get_mut(&tick) {
+            Some(cmds) => {
+                cmds.push_back(command);
+            }
+            None => {
+                self.map.insert(tick, VecDeque::from([command]));
+            }
+        }
+    }
+
+    pub(crate) fn remove(&mut self, tick: &u64) -> Option<VecDeque<fn(&mut Console)>> {
+        self.map.remove(&tick)
+    }
+}
