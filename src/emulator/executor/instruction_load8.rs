9c9186662d5927a872193bbb17c2ecfbb6c96dfa
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-04 18:49:04 -0500

    Implement register to register loading
    Finally came up with an implementation that's good enough... hopefully 🤞

--- src/emulator/executor/instruction_load8.rs
+++ src/emulator/executor/instruction_load8.rs
@@ -1,15 +1,17 @@
-use crate::emulator::{console::Console, instruction::Ld8, registers::Register8};
+use crate::emulator::{
+    console::Console, cpu::CPU, execution_queue::Command, instruction::Ld8, registers::Register8,
+};
 
 impl Console {
     pub(super) fn instruction_load8(&mut self, to: Ld8, from: Ld8) -> Option<u64> {
-        let from = match from {
-            Ld8::A => From::Register(Register8::A),
-            Ld8::B => From::Register(Register8::B),
-            Ld8::C => From::Register(Register8::C),
-            Ld8::D => From::Register(Register8::D),
-            Ld8::E => From::Register(Register8::E),
-            Ld8::H => From::Register(Register8::H),
-            Ld8::L => From::Register(Register8::L),
+        match from {
+            Ld8::A => self.register_from(to, Register8::A),
+            Ld8::B => self.register_from(to, Register8::B),
+            Ld8::C => self.register_from(to, Register8::C),
+            Ld8::D => self.register_from(to, Register8::D),
+            Ld8::E => self.register_from(to, Register8::E),
+            Ld8::H => self.register_from(to, Register8::H),
+            Ld8::L => self.register_from(to, Register8::L),
             Ld8::HL => todo!(),
             Ld8::HLPlus => todo!(),
             Ld8::HLMinus => todo!(),
@@ -19,9 +21,25 @@ impl Console {
             Ld8::U8 => todo!(),
             Ld8::FF00AddU8 => todo!(),
             Ld8::FF00AddC => todo!(),
-        };
+        }
+    }
 
-        let to = match to {
+    fn register_from(&mut self, to: Ld8, from: Register8) -> Option<u64> {
+        fn to_register(console: &mut Console, to: Register8, from: Register8) -> Option<u64> {
+            console.push_command(
+                3,
+                Command::SetRegister(CPU::set_register, console.cpu.get_register(from), to),
+            );
+            Some(4)
+        }
+
+        match Self::match_to(to) {
+            To::Register(register8) => to_register(self, register8, from),
+        }
+    }
+
+    fn match_to(to: Ld8) -> To {
+        match to {
             Ld8::A => To::Register(Register8::A),
             Ld8::B => To::Register(Register8::B),
             Ld8::C => To::Register(Register8::C),
@@ -38,9 +56,7 @@ impl Console {
             Ld8::U8 => todo!(),
             Ld8::FF00AddU8 => todo!(),
             Ld8::FF00AddC => todo!(),
-        };
-
-        todo!()
+        }
     }
 }
 
