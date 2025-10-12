cc883ef27077ca0b5dfb273350ab6595b833489d
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-11 21:37:57 -0500

    Create functions for add, sub, adc, sbc

--- src/emulator/commands/cpu_commands.rs
+++ src/emulator/commands/cpu_commands.rs
@@ -1,1 +1,22 @@
+use crate::emulator::{
+    cpu::CPU,
+    registers::{Flags, Register8},
+};
 
+impl CPU {
+    pub(crate) fn cpu_add(&mut self, value: u8, register: &Register8, flags: bool) {
+        todo!();
+    }
+
+    pub(crate) fn cpu_adc(&mut self, value: u8, register: Register8, flags: bool) {
+        todo!();
+    }
+
+    pub(crate) fn cpu_sub(&mut self, value: u8, register: Register8, flags: bool) {
+        todo!();
+    }
+
+    pub(crate) fn cpu_sbc(&mut self, value: u8, register: Register8, flags: bool) {
+        todo!();
+    }
+}
