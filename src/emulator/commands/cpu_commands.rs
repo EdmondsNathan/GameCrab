40c820eeb58118525015e7b77437712dcf1db14c
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-13 20:01:27 -0500

    Implement cpu_add

--- src/emulator/commands/cpu_commands.rs
+++ src/emulator/commands/cpu_commands.rs
@@ -5,7 +5,18 @@ use crate::emulator::{
 
 impl CPU {
     pub(crate) fn cpu_add(&mut self, value: u8, register: &Register8, flags: bool) {
-        todo!();
+        let original = self.get_register(register);
+        let (new, overflow) = original.overflowing_add(value);
+        self.set_register(new, register);
+
+        if !flags {
+            return;
+        }
+
+        self.set_flag(self.get_register(register) == 0, &Flags::Z);
+        self.set_flag(false, &Flags::N);
+        self.set_flag((original & 0xF) + (value & 0xF) > 0xF, &Flags::H);
+        self.set_flag(overflow, &Flags::C);
     }
 
         todo!();
