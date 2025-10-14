0222eedeecfe7d9db77b37e7b222509bae67cea9
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-13 20:01:37 -0500

    Implement cpu_adc

--- src/emulator/commands/cpu_commands.rs
+++ src/emulator/commands/cpu_commands.rs
@@ -19,8 +19,28 @@ impl CPU {
         self.set_flag(overflow, &Flags::C);
     }
 
-        todo!();
     pub(crate) fn cpu_adc(&mut self, value: u8, register: &Register8, flags: bool) {
+        let original = self.get_register(register);
+        let mut overflow;
+        let overflow2;
+        let new;
+        let mut value = value;
+        (value, overflow) = value.overflowing_add(match self.get_flag(&Flags::C) {
+            true => 1,
+            false => 0,
+        });
+        (new, overflow2) = original.overflowing_add(value);
+        overflow = overflow || overflow2;
+        self.set_register(new, register);
+
+        if !flags {
+            return;
+        }
+
+        self.set_flag(self.get_register(register) == 0, &Flags::Z);
+        self.set_flag(false, &Flags::N);
+        self.set_flag((original & 0xF) + (value & 0xF) > 0xF, &Flags::H);
+        self.set_flag(overflow, &Flags::C);
     }
 
     pub(crate) fn cpu_sub(&mut self, value: u8, register: &Register8, flags: bool) {
