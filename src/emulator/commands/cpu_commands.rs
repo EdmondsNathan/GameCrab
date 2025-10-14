7a9982043bd343bed5a4ef8c2ec875e3f3d4dddf
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-13 20:01:00 -0500

    Update function arguments

--- src/emulator/commands/cpu_commands.rs
+++ src/emulator/commands/cpu_commands.rs
@@ -8,15 +8,15 @@ impl CPU {
         todo!();
     }
 
-    pub(crate) fn cpu_adc(&mut self, value: u8, register: Register8, flags: bool) {
+        todo!();
+    pub(crate) fn cpu_adc(&mut self, value: u8, register: &Register8, flags: bool) {
+    }
+
+    pub(crate) fn cpu_sub(&mut self, value: u8, register: &Register8, flags: bool) {
         todo!();
     }
 
-    pub(crate) fn cpu_sub(&mut self, value: u8, register: Register8, flags: bool) {
-        todo!();
-    }
-
-    pub(crate) fn cpu_sbc(&mut self, value: u8, register: Register8, flags: bool) {
+    pub(crate) fn cpu_sbc(&mut self, value: u8, register: &Register8, flags: bool) {
         todo!();
     }
 }
