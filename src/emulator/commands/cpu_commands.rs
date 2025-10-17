c4b3aa9768bb6b97cd5abf021769eb3faa9600ce
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-17 17:56:13 -0500

    Implement cpu_sub

--- src/emulator/commands/cpu_commands.rs
+++ src/emulator/commands/cpu_commands.rs
@@ -56,6 +56,23 @@ impl CPU {
     }
 
     pub(crate) fn cpu_sbc(&mut self, value: u8, register: &Register8, flags: bool) {
-        todo!();
+        let original = self.get_register(register);
+        let (value_with_carry, overflow) = value.overflowing_add(match self.get_flag(&Flags::C) {
+            true => 1,
+            false => 0,
+        });
+
+        let (new, overflow2) = original.overflowing_sub(value_with_carry);
+        let overflow = overflow || overflow2;
+        self.set_register(new, register);
+
+        if !flags {
+            return;
+        }
+
+        self.set_flag(self.get_register(register) == 0, &Flags::Z);
+        self.set_flag(true, &Flags::N);
+        self.set_flag((original & 0xF) < (value_with_carry & 0xF), &Flags::H);
+        self.set_flag(overflow, &Flags::C);
     }
 }
