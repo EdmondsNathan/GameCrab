ce40347607c4a0b5737ef020a111f9bee7286c78
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-24 02:30:14 -0500

    Refactor Command Command is now a struct which has a source and destination parameter. It implements a method execute_command which will do what it says on the tin.

--- src/emulator/commands/command.rs
+++ src/emulator/commands/command.rs
@@ -1,12 +1,39 @@
-use crate::emulator::{
-    console::Console,
-    cpu::CPU,
-    registers::{Register16, Register8},
-};
+use crate::emulator::{console::Console, registers::Register8};
 
-pub(crate) enum Command {
-    Standard(fn(&mut Console)),
-    ReadWrite(fn(&mut Console, u16, &Register8), u16, Register8),
-    SetRegister(fn(&mut CPU, u8, &Register8), u8, Register8),
-    SetRegister16(fn(&mut CPU, u16, &Register16), u16, Register16),
+pub(crate) struct Command {
+    source: Source,
+    destination: Destination,
+}
+
+impl Command {
+    pub(crate) fn new(source: Source, destination: Destination) -> Command {
+        Command {
+            source,
+            destination,
+        }
+    }
+
+    pub(crate) fn execute_command(&self, console: &mut Console) {
+        let value = match &self.source {
+            Source::Register(register) => console.cpu.get_register(&register),
+            Source::Ram(address) => console.ram.fetch(*address),
+            Source::Value(value) => *value,
+        };
+
+        match &self.destination {
+            Destination::Register(register) => console.cpu.set_register(value, register),
+            Destination::Ram(address) => console.ram.set(value, *address),
+        }
+    }
+}
+
+pub(crate) enum Source {
+    Register(Register8),
+    Ram(u16),
+    Value(u8),
+}
+
+pub(crate) enum Destination {
+    Register(Register8),
+    Ram(u16),
 }
