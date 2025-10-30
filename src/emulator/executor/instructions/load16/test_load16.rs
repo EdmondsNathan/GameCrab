0add49a9ffb6d8c29bc25a43b599ef0dd3f2c78a
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-10-30 15:16:45 -0500

    Add remaing u16_to_register tests

--- src/emulator/executor/instructions/load16/test_load16.rs
+++ src/emulator/executor/instructions/load16/test_load16.rs
@@ -28,6 +28,8 @@ mod tests {
 
     #[test]
     fn de_u16() {
+        // (1, 0x100) is de_u16 at address x100
+        // the other two are the values to assign to registers E(50) and D(45)
         let mut console = init(vec![(0x11, 0x100), (50, 0x101), (45, 0x102)]);
 
         for n in 0..12 {
@@ -36,5 +38,47 @@ mod tests {
 
         assert_eq!(console.cpu.get_register(&Register8::D), 45);
         assert_eq!(console.cpu.get_register(&Register8::E), 50);
+    }
+
+    #[test]
+    fn hl_u16() {
+        // (1, 0x100) is hl_u16 at address x100
+        // the other two are the values to assign to registers L(50) and H(45)
+        let mut console = init(vec![(0x21, 0x100), (50, 0x101), (45, 0x102)]);
+
+        for n in 0..12 {
+            console.tick();
+        }
+
+        assert_eq!(console.cpu.get_register(&Register8::H), 45);
+        assert_eq!(console.cpu.get_register(&Register8::L), 50);
+    }
+
+    #[test]
+    fn sp_u16() {
+        // (1, 0x100) is sp_u16 at address x100
+        // the other two are the values to assign to registers E(50) and D(45)
+        let mut console = init(vec![(0x31, 0x100), (50, 0x101), (45, 0x102)]);
+
+        for n in 0..12 {
+            console.tick();
+        }
+
+        assert_eq!(console.cpu.get_register(&Register8::SpLow), 45);
+        assert_eq!(console.cpu.get_register(&Register8::SpHigh), 50);
+    }
+
+    #[test]
+    fn u16_sp() {
+        // (1, 0x100) is de_u16 at address x100
+        // the other two are the values to assign to registers E(50) and D(45)
+        let mut console = init(vec![(0x31, 0x100), (50, 0x101), (45, 0x102)]);
+
+        for n in 0..12 {
+            console.tick();
+        }
+
+        assert_eq!(console.cpu.get_register(&Register8::SpLow), 45);
+        assert_eq!(console.cpu.get_register(&Register8::SpHigh), 50);
     }
 }
