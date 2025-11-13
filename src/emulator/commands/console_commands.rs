11a0fa1ef9d796c5eb845daa3003be55cc85734c
Nathan Edmonds <edmondsnathan1998@gmail.com>
2025-11-12 21:22:18 -0600

    Remove dead code

--- src/emulator/commands/console_commands.rs
+++ src/emulator/commands/console_commands.rs
@@ -4,18 +4,6 @@ use crate::emulator::{
 };
 
 impl Console {
-    pub(crate) fn command_ram_to_register(&mut self, address: u16, register: &Register8) {
-        let value = self.ram.fetch(address);
-
-        self.cpu.set_register(value, register);
-    }
-
-    pub(crate) fn command_register_to_ram(&mut self, address: u16, register: &Register8) {
-        let value = self.cpu.get_register(register);
-
-        self.ram.set(value, address);
-    }
-
     pub(crate) fn command_increment_pc(&mut self) {
         self.cpu.set_register_16(
             self.cpu.get_register_16(&Register16::Pc) + 1,
