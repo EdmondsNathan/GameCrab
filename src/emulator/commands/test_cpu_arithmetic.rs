1af46df6f0d36d9e5a6a5c2170c4e6035c1a8a40
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-11-19 13:14:45 -0600

    Add test for cpu_sbc

--- src/emulator/commands/test_cpu_arithmetic.rs
+++ src/emulator/commands/test_cpu_arithmetic.rs
@@ -91,4 +91,44 @@ mod tests {
         assert!(cpu.get_flag(&Flags::H));
         assert!(!cpu.get_flag(&Flags::C));
     }
+
+    #[test]
+    fn sbc() {
+        let mut cpu = Cpu::new();
+        cpu.set_register(2, &Register8::A);
+
+        cpu.set_flag(true, &Flags::C);
+        cpu.cpu_sbc(1, &Register8::A, true);
+        assert_eq!(0, cpu.get_register(&Register8::A));
+        assert!(cpu.get_flag(&Flags::Z));
+        assert!(cpu.get_flag(&Flags::N));
+        assert!(!cpu.get_flag(&Flags::H));
+        assert!(!cpu.get_flag(&Flags::C));
+
+        cpu.set_flag(true, &Flags::C);
+        cpu.cpu_sbc(0, &Register8::A, true);
+        assert_eq!(255, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(cpu.get_flag(&Flags::N));
+        assert!(cpu.get_flag(&Flags::H));
+        assert!(cpu.get_flag(&Flags::C));
+
+        cpu.set_flag(false, &Flags::C);
+        cpu.set_register(0b00010000, &Register8::A);
+        cpu.cpu_sbc(0b00001000, &Register8::A, true);
+        assert_eq!(0b00001000, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(cpu.get_flag(&Flags::N));
+        assert!(cpu.get_flag(&Flags::H));
+        assert!(!cpu.get_flag(&Flags::C));
+
+        cpu.set_flag(true, &Flags::C);
+        cpu.set_register(0b00010000, &Register8::A);
+        cpu.cpu_sbc(0, &Register8::A, true);
+        assert_eq!(0b00001111, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(cpu.get_flag(&Flags::N));
+        assert!(cpu.get_flag(&Flags::H));
+        assert!(!cpu.get_flag(&Flags::C));
+    }
 }
