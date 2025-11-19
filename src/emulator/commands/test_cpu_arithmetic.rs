03ba4c414f57d167ba71fa9870ef0e907dd7a159
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-11-19 12:42:23 -0600

    Add tests

--- src/emulator/commands/test_cpu_arithmetic.rs
+++ src/emulator/commands/test_cpu_arithmetic.rs
@@ -9,8 +9,8 @@ mod tests {
     fn add() {
         let mut cpu = Cpu::new();
         cpu.set_register(254, &Register8::A);
-        cpu.cpu_add(1, &Register8::A, true);
 
+        cpu.cpu_add(1, &Register8::A, true);
         assert_eq!(255, cpu.get_register(&Register8::A));
         assert!(!cpu.get_flag(&Flags::Z));
         assert!(!cpu.get_flag(&Flags::N));
@@ -24,9 +24,40 @@ mod tests {
         assert!(cpu.get_flag(&Flags::H));
         assert!(cpu.get_flag(&Flags::C));
 
-        cpu.set_register(1, &Register8::A);
-        cpu.cpu_add(15, &Register8::A, true);
-        assert_eq!(16, cpu.get_register(&Register8::A));
+        cpu.set_register(0b00000001, &Register8::A);
+        cpu.cpu_add(0b00001111, &Register8::A, true);
+        assert_eq!(0b00010000, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(!cpu.get_flag(&Flags::N));
+        assert!(cpu.get_flag(&Flags::H));
+        assert!(!cpu.get_flag(&Flags::C));
+    }
+
+    #[test]
+    fn add_adc() {
+        let mut cpu = Cpu::new();
+        cpu.set_register(253, &Register8::A);
+
+        cpu.set_flag(true, &Flags::C);
+        cpu.cpu_adc(1, &Register8::A, true);
+        assert_eq!(255, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(!cpu.get_flag(&Flags::N));
+        assert!(!cpu.get_flag(&Flags::H));
+        assert!(!cpu.get_flag(&Flags::C));
+
+        cpu.set_flag(true, &Flags::C);
+        cpu.cpu_adc(1, &Register8::A, true);
+        assert_eq!(1, cpu.get_register(&Register8::A));
+        assert!(!cpu.get_flag(&Flags::Z));
+        assert!(!cpu.get_flag(&Flags::N));
+        assert!(cpu.get_flag(&Flags::H));
+        assert!(cpu.get_flag(&Flags::C));
+
+        cpu.set_register(0, &Register8::A);
+        cpu.set_flag(true, &Flags::C);
+        cpu.cpu_adc(0b00001111, &Register8::A, true);
+        assert_eq!(0b00010000, cpu.get_register(&Register8::A));
         assert!(!cpu.get_flag(&Flags::Z));
         assert!(!cpu.get_flag(&Flags::N));
         assert!(cpu.get_flag(&Flags::H));
