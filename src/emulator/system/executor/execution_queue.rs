5da78b534df0babb628de81131eb66834eec7767
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-11-22 01:37:32 -0600

    Update documentation

--- src/emulator/system/executor/execution_queue.rs
+++ src/emulator/system/executor/execution_queue.rs
@@ -7,10 +7,12 @@ pub(crate) struct ExecutionQueue {
 }
 
 impl ExecutionQueue {
+    /// Create a new execution queue object
     pub(crate) fn new() -> ExecutionQueue {
         Self::default()
     }
 
+    /// Push a command to a specific tick.
     pub(crate) fn push_command_absolute(&mut self, tick: u64, command: Command) {
         match self.map.get_mut(&tick) {
             Some(cmds) => {
@@ -22,10 +24,12 @@ impl ExecutionQueue {
         }
     }
 
+    /// Pop the queue from a specific tick.
     pub(crate) fn pop(&mut self, tick: &u64) -> Option<VecDeque<Command>> {
         self.map.remove(tick)
     }
 
+    /// Peek at the queue from a specific tick.
     pub(crate) fn peek(&mut self, tick: &u64) -> Option<&VecDeque<Command>> {
         self.map.get(tick)
     }
