f13756e28677b65ba050dd43d698a42fecb46a12
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-17 20:40:26 -0600

    Implement to_register_16 test

--- src/emulator/system/executor/instructions/load8/test_load8.rs
+++ src/emulator/system/executor/instructions/load8/test_load8.rs
@@ -13,7 +13,8 @@ fn init(memory_map: Vec<(u8, u16)>) -> Console {
 #[cfg(test)]
 mod go_from_register_8 {
     use crate::emulator::system::{
-        components::registers::Register8, executor::instructions::load8::test_load8::init,
+        components::registers::{Register16, Register8},
+        executor::instructions::load8::test_load8::init,
     };
 
     #[test]
@@ -22,10 +23,23 @@ mod go_from_register_8 {
         console.cpu.set_register(1, &Register8::B);
         console.cpu.set_register(0, &Register8::A);
 
-        for n in 0..8 {
+        for n in 0..4 {
             console.tick();
         }
 
         assert_eq!(console.cpu.get_register(&Register8::A), 1);
+    }
+
+    #[test]
+    fn to_register_16() {
+        let mut console = init(vec![(0x02, 0x100)]);
+        console.cpu.set_register(0x10, &Register8::A);
+        console.cpu.set_register_16(0x256, &Register16::Bc);
+
+        for n in 0..8 {
+            console.tick();
+        }
+
+        assert_eq!(console.ram.fetch(0x256), 0x10);
     }
 }
