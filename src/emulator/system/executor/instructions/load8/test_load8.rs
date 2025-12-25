4d9e25937e1286f94acdfa7eef54b5a08e853135
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-24 23:41:18 -0600

    Implement to_ff00_c test

--- src/emulator/system/executor/instructions/load8/test_load8.rs
+++ src/emulator/system/executor/instructions/load8/test_load8.rs
@@ -14,6 +14,7 @@ fn init(memory_map: Vec<(u8, u16)>) -> Console {
 mod go_from_register_8 {
     use crate::emulator::system::{
         components::registers::{Register16, Register8},
+        console,
         executor::instructions::load8::test_load8::init,
     };
 
@@ -92,5 +93,23 @@ mod go_from_register_8 {
 
         // Ensure the byte of ram at 0x0201 is loaded with register a's value
         assert_eq!(console.ram.fetch(0x0201), 0x05);
+    }
+
+    #[test]
+    fn to_ff00_c() {
+        // Copy the value of A into memory address 0xFF00 + register C
+
+        // 0xE2 LD (0xFF00 + C), A
+        let mut console = init(vec![(0xE2, 0x100)]);
+        console.cpu.set_register(0x03, &Register8::A);
+        console.cpu.set_register(0x05, &Register8::C);
+
+        // 0xE2 is 8 ticks
+        for n in 0..8 {
+            console.tick();
+        }
+
+        // Is Ram 0xFF00 + C == A?
+        assert_eq!(console.ram.fetch(0xFF05), 0x03);
     }
 }
