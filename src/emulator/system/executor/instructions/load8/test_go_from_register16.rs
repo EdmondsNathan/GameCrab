3da790d4142ca576021e078a685a46700dbba4c9
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-26 01:54:41 -0600

    Implement test for register16 to register

--- src/emulator/system/executor/instructions/load8/test_go_from_register16.rs
+++ src/emulator/system/executor/instructions/load8/test_go_from_register16.rs
@@ -1,1 +1,31 @@
+use crate::emulator::system::console::Console;
 
+fn init(memory_map: Vec<(u8, u16)>) -> Console {
+    let mut console = Console::new();
+
+    for memory in memory_map {
+        console.ram.set(memory.0, memory.1);
+    }
+
+    console
+}
+
+#[cfg(test)]
+mod go_from_register_16 {
+    use crate::emulator::system::{
+        components::registers::{Register16, Register8},
+        executor::instructions::load8::test_go_from_register16::init,
+    };
+
+    #[test]
+    fn to_register_8() {
+        let mut console = init(vec![(0x0A, 0x100), (0x03, 0x101)]);
+        console.cpu.set_register_16(0x101, &Register16::Bc);
+
+        for n in 0..8 {
+            console.tick();
+        }
+
+        assert_eq!(console.cpu.get_register(&Register8::A), 0x03);
+    }
+}
