6a75705614fcb407a40402f1cdec0eaffbce4cc4
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-26 03:29:09 -0600

    Update todo

--- src/emulator/system/executor/instructions/load8/go_from_u8.rs
+++ src/emulator/system/executor/instructions/load8/go_from_u8.rs
@@ -4,6 +4,7 @@ use crate::emulator::system::{
     executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
 };
 
+// TAG_TODO
 impl Console {
     pub(super) fn go_from_u8(&mut self, to: To, from: u8) -> Option<u64> {
         fn to_register8(console: &mut Console, to: Register8, from: u8) -> Option<u64> {
