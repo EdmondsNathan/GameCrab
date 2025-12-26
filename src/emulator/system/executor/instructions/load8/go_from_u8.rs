c3f5c1ac13f079c1fc6688f408022b73dce4527e
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-26 03:54:06 -0600

    Remove unused instructions

--- src/emulator/system/executor/instructions/load8/go_from_u8.rs
+++ src/emulator/system/executor/instructions/load8/go_from_u8.rs
@@ -15,29 +15,13 @@ impl Console {
             todo!();
         }
 
-        fn to_hl(console: &mut Console, to: Hl, from: u8) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_u8(console: &mut Console, from: u8) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_u16(console: &mut Console, from: u8) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_ff00(console: &mut Console, to: Ff00, from: u8) -> Option<u64> {
-            todo!();
-        }
-
         match to {
             To::Register8(register8) => to_register8(self, register8, from),
             To::Register16(register16) => to_register16(self, register16, from),
-            To::Hl(hl) => to_hl(self, hl, from),
-            To::U8 => to_u8(self, from),
-            To::U16 => to_u16(self, from),
-            To::Ff00(ff00) => to_ff00(self, ff00, from),
+            To::Hl(hl) => panic!("Invalid instruction!"),
+            To::U8 => panic!("Invalid instruction!"),
+            To::U16 => panic!("Invalid instruction!"),
+            To::Ff00(ff00) => panic!("Invalid instruction!"),
         }
     }
 }
