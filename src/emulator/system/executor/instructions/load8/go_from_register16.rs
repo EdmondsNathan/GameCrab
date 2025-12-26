b06bc6a8b1d679e203aac01c07ee92030a4dcc40
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-25 21:44:42 -0600

    Remove invalid instructions

--- src/emulator/system/executor/instructions/load8/go_from_register16.rs
+++ src/emulator/system/executor/instructions/load8/go_from_register16.rs
@@ -9,35 +9,19 @@ impl Console {
         fn to_register8(console: &mut Console, to: Register8, from: Register16) -> Option<u64> {
             // TODO
             todo!();
-        }
 
-        fn to_register16(console: &mut Console, to: Register16, from: Register16) -> Option<u64> {
-            todo!();
-        }
 
-        fn to_hl(console: &mut Console, to: Hl, from: Register16) -> Option<u64> {
-            todo!();
-        }
 
-        fn to_u8(console: &mut Console, from: Register16) -> Option<u64> {
-            todo!();
-        }
 
-        fn to_u16(console: &mut Console, from: Register16) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_ff00(console: &mut Console, to: Ff00, from: Register16) -> Option<u64> {
-            todo!();
         }
 
         match to {
             To::Register8(register8) => to_register8(self, register8, from),
-            To::Register16(register16) => to_register16(self, register16, from),
-            To::Hl(hl) => to_hl(self, hl, from),
-            To::U8 => to_u8(self, from),
-            To::U16 => to_u16(self, from),
-            To::Ff00(ff00) => to_ff00(self, ff00, from),
+            To::Register16(register16) => panic!("Invalid instruction!"),
+            To::Hl(hl) => panic!("Invalid instruction!"),
+            To::U8 => panic!("Invalid instruction!"),
+            To::U16 => panic!("Invalid instruction!"),
+            To::Ff00(ff00) => panic!("Invalid instruction!"),
         }
     }
 }
