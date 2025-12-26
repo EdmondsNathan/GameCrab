0c3bfc52b4e2ab26de1a44dcbd1036ed48c46db4
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-26 01:45:02 -0600

    Implement to_register8

--- src/emulator/system/executor/instructions/load8/go_from_register16.rs
+++ src/emulator/system/executor/instructions/load8/go_from_register16.rs
@@ -1,18 +1,42 @@
-use crate::emulator::system::{
-    components::registers::{Register16, Register8},
-    console::Console,
-    executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+use crate::emulator::{
+    commands::command::{
+        Command::{Read, Update},
+        Destination, Source,
+    },
+    system::{
+        components::registers::{Register16, Register8},
+        console::Console,
+        executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+    },
 };
 
 impl Console {
     pub(super) fn go_from_register16(&mut self, to: To, from: Register16) -> Option<u64> {
         fn to_register8(console: &mut Console, to: Register8, from: Register16) -> Option<u64> {
-            // TODO
-            todo!();
+            // Push from value into xy so we can use the value inside of closure
+            console
+                .cpu
+                .set_register_16(console.cpu.get_register_16(&from), &Register16::Xy);
 
+            console.push_command(
+                3,
+                Update(|console: &mut Console| {
+                    console.cpu.set_register_16(
+                        console.cpu.get_register_16(&Register16::Xy),
+                        &Register16::Bus,
+                    );
+                }),
+            );
 
+            console.push_command(
+                4,
+                Read(
+                    Source::RamFromRegister(Register16::Bus),
+                    Destination::Register(to),
+                ),
+            );
 
-
+            Some(8)
         }
 
         match to {
