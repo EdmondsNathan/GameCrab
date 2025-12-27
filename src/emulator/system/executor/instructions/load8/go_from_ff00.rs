b3a5f29c53a8a579161bcf7307ab3495e69e5162
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-27 14:50:11 -0600

    Implement plus_c

--- src/emulator/system/executor/instructions/load8/go_from_ff00.rs
+++ src/emulator/system/executor/instructions/load8/go_from_ff00.rs
@@ -1,7 +1,13 @@
-use crate::emulator::system::{
-    components::registers::{Register16, Register8},
-    console::Console,
-    executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+use crate::emulator::{
+    commands::command::{
+        Command::{Read, Update},
+        Destination, Source,
+    },
+    system::{
+        components::registers::{Register16, Register8},
+        console::Console,
+        executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+    },
 };
 
 //TAG_TODO
@@ -9,11 +15,44 @@ impl Console {
     pub(super) fn go_from_ff00(&mut self, to: To, from: Ff00) -> Option<u64> {
         fn to_register8(console: &mut Console, to: Register8, from: Ff00) -> Option<u64> {
             match from {
-                Ff00::C => return plus_c(),
+                Ff00::C => return plus_c(console),
                 Ff00::U8 => return plus_u8(),
             }
 
-            fn plus_c() -> Option<u64> {
+            fn plus_c(console: &mut Console) -> Option<u64> {
+                console.push_command(
+                    3,
+                    Read(
+                        Source::Register(Register8::A),
+                        Destination::Register(Register8::Y),
+                    ),
+                );
+
+                console.push_command(
+                    4,
+                    Update(|console: &mut Console| {
+                        console.cpu.set_register(0xFF, &Register8::X);
+                    }),
+                );
+
+                console.push_command(
+                    5,
+                    Update(|console: &mut Console| {
+                        console.cpu.set_register_16(
+                            console.cpu.get_register_16(&Register16::Xy),
+                            &Register16::Bus,
+                        );
+                    }),
+                );
+
+                console.push_command(
+                    6,
+                    Read(
+                        Source::RamFromRegister(Register16::Bus),
+                        Destination::Register(Register8::A),
+                    ),
+                );
+
                 Some(8)
             }
 
