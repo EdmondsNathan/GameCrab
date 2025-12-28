94f819e5199abbe2885c67e8c35e8bec241b3784
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2025-12-28 02:34:11 -0600

    Implement go_from_hl

--- src/emulator/system/executor/instructions/load8/go_from_hl.rs
+++ src/emulator/system/executor/instructions/load8/go_from_hl.rs
@@ -1,43 +1,63 @@
-use crate::emulator::system::{
-    components::registers::{Register16, Register8},
-    console::Console,
-    executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+use crate::emulator::{
+    commands::command::{
+        Command::{Read, Update},
+        Destination, Source,
+    },
+    system::{
+        components::registers::{Register16, Register8},
+        console::Console,
+        executor::instructions::load8::instruction_load8::{Ff00, Hl, To},
+    },
 };
 
-//TAG_TODO
 impl Console {
     pub(super) fn go_from_hl(&mut self, to: To, from: Hl) -> Option<u64> {
         fn to_register8(console: &mut Console, to: Register8, from: Hl) -> Option<u64> {
-            todo!();
-        }
+            console.push_command(
+                3,
+                Update(|console: &mut Console| {
+                    console.cpu.set_register_16(
+                        console.cpu.get_register_16(&Register16::Hl),
+                        &Register16::Bus,
+                    );
+                }),
+            );
 
-        fn to_register16(console: &mut Console, to: Register16, from: Hl) -> Option<u64> {
-            todo!();
-        }
+            console.push_command(
+                4,
+                Read(
+                    Source::RamFromRegister(Register16::Hl),
+                    Destination::Register(to),
+                ),
+            );
 
-        fn to_hl(console: &mut Console, to: Hl, from: Hl) -> Option<u64> {
-            todo!();
-        }
+            match from {
+                Hl::Plus => console.push_command(
+                    5,
+                    Update(|console: &mut Console| {
+                        console.cpu.set_register_16(
+                            console.cpu.get_register_16(&Register16::Hl) + 1,
+                            &Register16::Hl,
+                        );
+                    }),
+                ),
+                Hl::Minus => console.push_command(
+                    5,
+                    Update(|console: &mut Console| {
+                        console.cpu.set_register_16(
+                            console.cpu.get_register_16(&Register16::Hl) - 1,
+                            &Register16::Hl,
+                        );
+                    }),
+                ),
+            }
 
-        fn to_u8(console: &mut Console, from: Hl) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_u16(console: &mut Console, from: Hl) -> Option<u64> {
-            todo!();
-        }
-
-        fn to_ff00(console: &mut Console, to: Ff00, from: Hl) -> Option<u64> {
-            todo!();
+            Some(8)
         }
 
         match to {
             To::Register8(register8) => to_register8(self, register8, from),
-            To::Register16(register16) => to_register16(self, register16, from),
-            To::Hl(hl) => to_hl(self, hl, from),
-            To::U8 => to_u8(self, from),
-            To::U16 => to_u16(self, from),
-            To::Ff00(ff00) => to_ff00(self, ff00, from),
+            _ => panic!("Invalid instruction!"),
         }
     }
 }
