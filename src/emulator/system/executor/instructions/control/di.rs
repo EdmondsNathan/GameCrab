225d5f841338516973f000c219da67db6be1bfeb
Nathan Edmonds <nathan.edmonds.business@gmail.com>
2026-01-06 00:46:35 -0600

    Implement di and ei

---
+++ src/emulator/system/executor/instructions/control/di.rs
@@ -0,0 +1,42 @@
+use crate::emulator::{commands::command::Command::Update, system::console::Console};
+
+impl Console {
+    pub(super) fn di(&mut self) -> Option<u64> {
+        self.push_command(
+            3,
+            Update(|console: &mut Console| {
+                console.cpu.set_interrupt(false);
+            }),
+        );
+        Some(4)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::emulator::system::console::{self, Console};
+
+    fn init(memory_map: Vec<(u8, u16)>) -> Console {
+        let mut console = Console::new();
+
+        for memory in memory_map {
+            console.ram.set(memory.0, memory.1);
+        }
+
+        console
+    }
+
+    #[test]
+    fn di() {
+        let mut console = init(vec![(0xF3, 0x100)]);
+        console.cpu.set_interrupt(true);
+
+        assert!(console.cpu.get_interrupt());
+
+        for n in 0..4 {
+            console.tick();
+        }
+
+        assert!(!console.cpu.get_interrupt());
+    }
+}
